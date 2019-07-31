5c136dadbbc0c9b4d94dc3c8bbc620894db8441c
machuanjia <yanshi0429@126.com>
2019-07-31 10:01:27 +0800

    feat: add prender

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ngx-render
+
+## 启动 npm start
+
+## 参考 https://segmentfault.com/a/1190000014596299#articleHeader1
+
---
+++ server.js
@@ -0,0 +1,7 @@
+const prerender = require("prerender");
+const server = prerender();
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.blockResources());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+server.start();
